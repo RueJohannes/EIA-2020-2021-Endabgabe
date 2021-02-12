c585d9648a16fd3daa6c8d9c53027351359cf76a
RueJohannes <derh4nn3s@gmail.com>
2021-02-12 21:03:55 +0100

    Ordnerstruktur geändert

--- js/Main/Main.ts
+++ js/Main/Main.ts
@@ -33,7 +33,7 @@ namespace Firework {
     inputParticleQuantity.addEventListener("change", startMeter);
     window.setInterval(update, 20);
 
-    backgroundImage.src = "./../images/wsb_logo_bearbeitet.png";
+    backgroundImage.src = "images/wsb_logo_bearbeitet.png";
   }
 
   function createObject(_event: MouseEvent): void {
